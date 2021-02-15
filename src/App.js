537c2feb3372fda2ae1102138763e6251b776226
nithishkumar <balajinithishkumar>
2021-02-15 21:17:01 +0530

    new version

--- src/App.js
+++ src/App.js
@@ -1,27 +1,76 @@
-import React from "react";
-import { Counter } from "./features/counter/Counter";
+import React, { useEffect } from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./Screen/HomeScreen";
 import requests from "./Requests";
 import Row from "./Row";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LoginScreen from "./Screen/LoginScreen";
+import { auth } from "./Screen/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectuser } from "./features/counterSlice";
+import ProfileScreen from "./Screen/ProfileScreen";
 function App() {
+  const user = useSelector(selectuser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        console.log(authUser);
+        dispatch(
+          login({
+            uid: authUser.uid,
+            email: authUser.email,
+          })
+        );
+      } else {
+        dispatch(logout());
+        console.log(authUser);
+      }
+    });
+    return unSubscribe;
+  }, []);
+  console.log(user);
   return (
     <div className="app">
-      <HomeScreen />
-      <Row
-        title="NETFLIX ORIGINAL"
-        FetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      ></Row>
+      <Router>
+        {!user ? (
+          <Route path="/loginscreen">
+            <LoginScreen />
+          </Route>
+        ) : (
+          <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
+            <Route path="/">
+              <HomeScreen />
+              <Row
+                title="NETFLIX ORIGINAL"
+                FetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+              ></Row>
 
-      <Row title="HorrorMovies" FetchUrl={requests.fetchHorrorMovies}></Row>
+              <Row
+                title="HorrorMovies"
+                FetchUrl={requests.fetchHorrorMovies}
+              ></Row>
 
-      <Row title="Trending" FetchUrl={requests.fetchTrending}></Row>
+              <Row title="Trending" FetchUrl={requests.fetchTrending}></Row>
 
-      <Row title="RumanceMovie" FetchUrl={requests.fetchRumanceMovie}></Row>
+              <Row
+                title="RumanceMovie"
+                FetchUrl={requests.fetchRumanceMovie}
+              ></Row>
 
-      <Row title="ActionMovies" FetchUrl={requests.fetchActionMovies}></Row>
+              <Row
+                title="ActionMovies"
+                FetchUrl={requests.fetchActionMovies}
+              ></Row>
+            </Route>
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useHistory,Link } from "react-router-dom";
 import "./Nav.css";
 function Nav() {
   const [show, handleShow] = useState(false);
-
+  const history = useHistory();
   const tranctionNavBar = () => {
     if (window.scrollY > 220) {
       handleShow(true);
@@ -22,13 +23,16 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav_black"}`}>
       <div className="nav_contents">
-        <img
-          className="nav_logo"
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-          alt=""
-        ></img>
+        <Link to="/">
+          <img
+            className="nav_logo"
+            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            alt=""
+          ></img>
+        </Link>
 
         <img
+          onClick={() => history.push("./profile")}
           className="nav_avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt=""
