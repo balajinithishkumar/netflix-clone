ce9aecb3a075f5d0032aa260ec0a6e5e51032190
nithishkumar <balajinithishkumar>
2021-02-16 17:48:24 +0530

    not deployed version

--- src/Screen/firebase.js
+++ src/Screen/firebase.js
@@ -1,6 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCQyHme2Ra0eNgqRNGLBjYMSHTv888kG-s",
   authDomain: "netflix-clone-nithu.firebaseapp.com",
@@ -10,7 +8,6 @@ const firebaseConfig = {
   appId: "1:111169524089:web:eb0f35d7867cba0735dfa8",
   measurementId: "G-60M63D4K4T",
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
